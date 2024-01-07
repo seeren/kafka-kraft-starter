668454923e40cc489ca76f49f5b452fac888d2eb
seeren <consultant@seeren.fr>
2024-01-07 21:52:03 +0100

    Create cluster id to tmp directory

--- .docker/scripts/create_cluster_id.sh
+++ .docker/scripts/create_cluster_id.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-file_path="/tmp/clusterID/.env"
+kafka-storage random-uuid > /tmp/clusterID
 
-if [ ! -f "$file_path" ]; then
-  echo "CLUSTER_ID=$(/bin/kafka-storage random-uuid)" > /tmp/clusterID/.env
-  echo "Env file with CLUSTER_ID has been created..."
-fi
+echo "CLUSTER_ID has been created"
