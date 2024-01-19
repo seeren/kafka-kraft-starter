fd038ed11e62739f711d06964b2487a38c403763
seeren <consultant@seeren.fr>
2024-01-19 20:48:44 +0100

    Add service skeleton

---
+++ services/customer/src/main.ts
@@ -0,0 +1,25 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { useContainer } from 'class-validator';
+
+import { AppModule } from 'src/app/app.module';
+
+declare const module: {
+  hot: {
+    accept: () => void;
+    dispose: (callable: () => Promise<void>) => void;
+  };
+};
+
+const bootstrap = async () => {
+  const application = await NestFactory.create(AppModule);
+  useContainer(application.select(AppModule), { fallbackOnErrors: true });
+  application.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  await application.listen(3001);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => application.close());
+  }
+};
+
+bootstrap();
